73a544fcae2ea4d840a7906076cdb9cd7652becd
Francesco Paolucci <francescopaolucci55@gmail.com>
2021-01-19 10:07:43 +0100

    Header

---
+++ header.php
@@ -0,0 +1,83 @@
+<head>
+  <title>GC calisthenics Academy</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Here is a precise description of my awesome banana webpage.">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="style.css" />
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+
+</head>
+<body>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-lg">
+    <nav class="navbar navbar-expand-sm bg-dark navbar-dark fixed-top">
+    <a class="navbar-brand" href="#">GC Academy</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="about.php">Coach</a>
+        </li>
+        <li class="nav-item">
+        <a class="nav-link" href="pricing.php">Community</a>
+        </li>
+        <li class="nav-item">
+        <a class="nav-link" href="Contact.php">Eventi</a>
+        </li>
+        <li class="nav-item">
+        <a class="nav-link" href="Signup.php">Contatti</a>
+        </li>
+        <li class="nav-item">
+        <a class="nav-link" href="Signup.php">Entra nell'academy</a>
+        </li>
+    </ul>
+    
+  </div>
+</nav>
+            </div></div>
+       
+    </div>
+
+
+
+       <div class="container">
+     
+ 
+
+  <img src="./img/head.jpg" alt="Notebook" style="width:100%;">
+
+  
+  <script>
+if(document.location.href.search("ProjectWork/index.php")!=-1){
+  var element = document.getElementById("homeid")
+  element.style.setProperty('color', 'white', 'important');
+}
+  if(document.location.href.search("/ProjectWork/pricing.php")!=-1){
+    var element = document.getElementById("pricingid")
+  document.getElementById("pricingid").style.color= "white";
+  
+  }
+  if(document.location.href.search("/ProjectWork/Contactus.php")!=-1){
+    var element = document.getElementById("contactusid")
+    element.style.setProperty('color', 'white', 'important');
+  
+  }
+  if(document.location.href.search("/ProjectWork/aboutus.php")!=-1){
+    var element = document.getElementById("aboutusid")
+    element.style.setProperty('color', 'white', 'important');
+  
+  }
+
+</script>
