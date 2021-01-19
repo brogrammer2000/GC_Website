5d90e5e6f8a5e6d351b155d1ea1c1369ca481624
P1811 <p1811@student.jamk.fi>
2021-01-19 15:16:00 +0530

    Update footer.php

--- footer.php
+++ footer.php
@@ -12,7 +12,8 @@
 
 <div class="footer">
     <div class="col-sm-12">
-    <p> Help us to Grow More... Contact us:)</p>
+    <p> Help us to Grow More</p>
+    <p>Contact us</p>
     <p> GC ACADEMY </p>
     <p><a id="tel" href="tel:041752164">041752164</a> </p>
     <p><a id="email" href="mailto:gcacademy@gmail.com">gcacademy@gmail.com</a></p>
