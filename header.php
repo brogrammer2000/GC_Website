38caaf732411acd106530a8ec1b6fe21c8410154
P1811 <p1811@student.jamk.fi>
2021-01-20 15:20:51 +0530

    integration

--- header.php
+++ header.php
@@ -1,4 +1,4 @@
-<head>
+<!--<head>
   <title>Gabriele Cerpi Academy</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -35,6 +35,57 @@
             <li><a href="#">Prezzi e Configurazioni</a></li>
         </ul>
     </div>
-</nav>
-</body>
+</nav>-->
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1"> <!-- mobile first -->
+  <!-- Latest compiled and minified CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+  <!-- jQuery library -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <!-- Popper JS -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <!-- Latest compiled JavaScript -->
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+  <!--StyleShhet link-->
+  <link rel="stylesheet" href="frontpage.css">
+
+
+
+</head>
+
+<body>
+  <div class="row">
+    <div class="col-sm-36" style="margin-left: 15%; width: 100%;">
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+              <a style="margin-left: 20px; font-size: large;" class="nav-link" href="breakfast.html">
+                Contatti </a style="margin-left: 10px;">
+            </li>
+            <li class="nav-item">
+              <a style="margin-left: 20px; font-size: large; margin-right: 40px;" class="nav-link" href="lunch.html">
+                Chi sono </a style="margin-left: 10px;}">
+            </li>
+            <li class="nav-item">
+              <img id="logo" width="100" height="100" alt="100" class="header-image" src="Images\Logonb.png">
+              <!---->
+            </li>
+            <li class="nav-item">
+              <a style="margin-left: 50px; font-size: large;" class="nav-link" href="dinner.html">
+                L'Academy </a>
+            </li>
+            <li class="nav-item">
+              <a style="margin-left: 20px; font-size: large;" class="nav-link" href="dinner.html">
+                Prezzi e Configurazioni </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  </div>
--- index.php
+++ index.php
@@ -1,28 +1,35 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-      <!--Meta tags start-->
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Testing</title>
-    <!--Meta tags end-->
 
-    <!-- Bootstrap CSS -->
-    <link
-      rel="stylesheet"
-      href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-      integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
-      crossorigin="anonymous"
-    />
-    <!--Bootstrap ends here-->
+<head>
+  <!--Meta tags start-->
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Testing</title>
+  <!--Meta tags end-->
 
-    <!--CSS link-->
-    <link rel="stylesheet" href="frontpage.css" />
-  </head>
-  <body>
-    <h1>This is a heading</h1>
-    <p><a href="second.html">new page</a></p>
-    <?php include "footer.php"
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous" />
+  <!--Bootstrap ends here-->
+
+  <!--CSS link-->
+  <link rel="stylesheet" href="frontpage.css" />
+</head>
+
+<body>
+  <div class="container">
+    <?php include "header.php"
     ?>
-  </body>
+  </div>
+  <div class="row">
+    <div class="col-sm-12" style="text-align: center;">
+      <p>testing</p>
+    </div>
+  </div>
+
+  <?php include "footer.php"
+  ?>
+
+</body>
+
 </html>
